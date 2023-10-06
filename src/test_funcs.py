de45abf035971388aaa906daec2ff9f5c67634f6
grnimrod <nimrod.grandpierre@gmail.com>
2023-10-06 09:29:46 +0200

    Test functions finalized

--- src/test_funcs.py
+++ src/test_funcs.py
@@ -11,16 +11,16 @@ from funcs import (
 
 
 def test_prod():
-    assert prod(1, 2, 3) == 'TEST ME'
+    assert prod(1, 2, 3) == 6
 
 
 def test_prod2():
-    assert prod2(2) == 'TEST ME'
+    assert prod2(2) == 20
 
 
 def test_longest():
-    assert longest([1, 2, 3], [4, 5]) == 'TEST ME'
+    assert longest([1, 2, 3], [4, 5]) == [1, 2, 3]
 
 
 def test_dist():
-    assert dist((1, 2), (3, 4)) == 'TEST ME'
+    assert dist((1, 2), (3, 4)) == 8 ** 0.5
